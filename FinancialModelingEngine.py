58b62864ba672c22000ac13100a4e7df9b923750
Zach <zach@pod.io>
2022-10-14 14:54:17 -0700

    Add dataframe view

--- FinancialModelingEngine.py
+++ FinancialModelingEngine.py
@@ -2,14 +2,14 @@
 ## Variables
 yearsToRun = 50
 monthlyAssetAppreciation = 1.0083333
-debtYearlyInterest = .025
+debtYearlyInterest = .08
 
 import pandas as pd
 import locale
-from bokeh.layouts import row
+from bokeh.layouts import row, gridplot
 from bokeh.plotting import figure, output_file, show, curdoc
-from bokeh.models import ColumnDataSource, NumeralTickFormatter
-from bokeh.models import HoverTool
+from bokeh.models import ColumnDataSource, NumeralTickFormatter, HoverTool
+from bokeh.models.widgets import DataTable, TableColumn
 bokeh_doc = curdoc()
 # Loop Conditions
 monthsToRun = yearsToRun * 12
@@ -40,7 +40,7 @@ for month in range(0, monthsToRun):
         cashflow = pd.concat([cashflow, newCashFlow])
       # Assets
       if (event.Asset == 1):
-        newAsset = pd.DataFrame([{'Name': event.Name, 'Value': event.Debt}])
+        newAsset = pd.DataFrame([{'Month': month, 'Name': event.Name, 'Value': event.Debt}])
         portfolioAssets = pd.concat([portfolioAssets, newAsset])
       # Debt
       newDebt = int(event.Debt)
@@ -94,7 +94,6 @@ debtFigure.line(debt_axis, debtDf['Amount'].cumsum(), legend_label="Debt Amount"
 
 cashflow_axis = cashflow['Month'].unique()
 cashflow_y_axis = cashflow.groupby(['Month']).sum(numeric_only=True).cumsum()['Amount']
-print(cashflow_y_axis)
 # create plot for cashflow
 cashflowFigure = figure(title="Pre-investment Cashflow", x_axis_label='Month', y_axis_label='Cashflow', tools='pan')
 crosshair = HoverTool(tooltips=[("Month", "$x"), ("Value", "$y{$0,0}")], formatters={'value': 'numeral'})
@@ -103,7 +102,15 @@ cashflowFigure.yaxis[0].formatter = NumeralTickFormatter(format="$0,0")
 cashflowFigure.line(cashflow_axis, cashflow_y_axis, legend_label="Cashflow", line_width=2)
 
 
-bokeh_doc.add_root(row(portfolioValueFigure, debtFigure, cashflowFigure))
+cashflowColumns = [TableColumn(field=Ci, title=Ci) for Ci in cashflow.columns]
+cashflowTable = DataTable(source=ColumnDataSource(cashflow), columns=cashflowColumns)
+debtColumns = [TableColumn(field=Ci, title=Ci) for Ci in debtDf.columns]
+debtTable = DataTable(source=ColumnDataSource(debtDf), columns=debtColumns)
+portfolioColumns = [TableColumn(field=Ci, title=Ci) for Ci in portfolioAssets.columns]
+portfolioTable = DataTable(source=ColumnDataSource(portfolioAssets), columns=portfolioColumns)
+
+grid = gridplot([[portfolioValueFigure, debtFigure, cashflowFigure], [portfolioTable, debtTable, cashflowTable]]) 
+bokeh_doc.add_root(grid)
 
 
 
