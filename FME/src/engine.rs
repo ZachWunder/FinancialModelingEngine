f046906061de7c913da0de6d282a7528bac1c4b8
Zach <zach@pod.io>
2022-10-26 14:41:14 -0700

    Add tests

--- FME/src/engine.rs
+++ FME/src/engine.rs
@@ -1,5 +1,116 @@
-// use crate::{Cashflow, Portfolio, Event};
+use crate::{Cashflow, Portfolio, Event, types::MonthlyOutput, Asset, Debt};
+use crate::{utils};
 
-// fn run_engine() -> (Cashflow, Portfolio) {
+pub fn run_engine(years_to_run: u32, mut cashflow: Vec<Cashflow>, events: Vec<Event>, mut assets: Portfolio ) -> Vec<MonthlyOutput> {
+  let months_to_run: u32 = years_to_run * 12;
+  // 8% divided monthly
+  const MONTHLY_DEBT_INTEREST_RATE: f32 = 0.08 / 12.0;
+  // 10% divided monthly
+  const MONTHLY_ASSET_APPRECIATION: f32 = 0.10 / 12.0;
 
-// }
+  let mut debt: Vec<Debt> = Vec::new();
+  let mut monthly_outputs: Vec<MonthlyOutput> = Vec::new();
+
+  // Engine Loop
+  for month in 0..months_to_run {
+      // Handle Events
+      let current_events: Vec<&Event> = events.iter().filter(|event| event.month == month).collect();
+      if !current_events.is_empty() {
+          for current_event in current_events {
+              // Cashflow
+              cashflow.push( Cashflow { month: month, name: current_event.name.clone(), amount: current_event.cashflow });
+              // Assets
+              if current_event.asset {
+                  assets.push( Asset { month: month, name: current_event.name.clone(), value: current_event.debt });
+              }
+              // Debt
+              if current_event.debt != 0.0 {
+                  debt.push(Debt { month: month, amount: current_event.debt });
+                  let interest_payment = current_event.debt * MONTHLY_DEBT_INTEREST_RATE;
+                  // Add interest payments (negative) to cashflow
+                  cashflow.push( Cashflow { month: month, name: current_event.name.clone(), amount: -interest_payment });
+              }
+          }
+      }
+      
+      // Add monthly gains to assets
+      let mut total_portfolio_value: f32 = 0.0;
+      for asset in &assets {
+          total_portfolio_value += asset.value
+      }
+      let monthly_gain = total_portfolio_value * MONTHLY_ASSET_APPRECIATION;
+      assets.push( Asset { month: month, name: format!("Appreciation {}", month), value: monthly_gain });
+
+      // Divert excess cashflow to assets (securities)
+      let mut monthly_excess_cashflow:f32 = 0.0;
+      for val in &cashflow {
+          monthly_excess_cashflow += val.amount;
+      }
+
+      assets.push( Asset { month: month, name: format!("Excess Cashflow {}", month), value: monthly_excess_cashflow });
+
+      let portfolio_total = utils::sum_portfolio(&assets) as i64;
+      let debt_total = utils::sum_debt(&debt) as i64;
+      let output = MonthlyOutput {
+          month: month,
+          portfolio_sum: portfolio_total,
+          debt_sum: debt_total,
+          cashflow_sum: monthly_excess_cashflow as i64
+      };
+      monthly_outputs.push(output)
+  }
+  return monthly_outputs;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple() {
+      let income = Cashflow {
+        month: 0,
+        name: "Income".to_string(),
+        amount: 5000.00
+      };
+      let mut cashflow: Vec<Cashflow> = Vec::new();
+      cashflow.push(income);
+
+      let events = Vec::new();
+      let assets = Vec::new();
+
+      let output = run_engine(5, cashflow, events, assets);
+      let last_month = output.last().unwrap();
+      
+      assert_eq!(last_month.portfolio_sum, 387_185);
+    }
+    
+    #[test]
+    fn debt() {
+      let income = Cashflow {
+        month: 0,
+        name: "Income".to_string(),
+        amount: 5000.00
+      };
+      let mut cashflow: Vec<Cashflow> = Vec::new();
+      cashflow.push(income);
+
+      let buy_property = Event {
+        name: "Buy Property".to_string(),
+        month: 12,
+        cashflow: 0f32,
+        debt: 30000f32,
+        asset: true
+      };
+
+      let mut events = Vec::new();
+      events.push(buy_property);
+      let assets = Vec::new();
+
+      let output = run_engine(5, cashflow, events, assets);
+      let last_month = output.last().unwrap();
+      
+      assert_eq!(last_month.cashflow_sum, 4_800);
+      assert_eq!(last_month.portfolio_sum, 420_121);
+    }
+}
--- FME/src/main.rs
+++ FME/src/main.rs
@@ -2,72 +2,23 @@ mod utils;
 mod engine;
 mod types;
 
-use std::{env, fs, io};
+use std::{env, io};
 use types::{Event, Cashflow, Asset, Portfolio, Debt, MonthlyOutput};
 
-const YEARS_TO_RUN: u32 = 20 * 12;
-// 8% divided monthly
-const MONTHLY_DEBT_INTEREST_RATE: f32 = 0.08 / 12.0;
-// 10% divided monthly
-const MONTHLY_ASSET_APPRECIATION: f32 = 0.10 / 12.0;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let folder = &args[1];
     let events: Vec<Event> = utils::read_file(format!("./inputs/{}/events.csv", folder));
-    let mut cashflow: Vec<Cashflow> = utils::read_file(format!("./inputs/{}/cashflow.csv", folder));
-    let mut portfolio: Vec<Asset> = utils::read_file(format!("./inputs/{}/portfolio.csv", folder));
-    let mut debt: Vec<Debt> = Vec::new();
-
+    let cashflow: Vec<Cashflow> = utils::read_file(format!("./inputs/{}/cashflow.csv", folder));
+    let portfolio: Vec<Asset> = utils::read_file(format!("./inputs/{}/portfolio.csv", folder));
     let mut std_wtr = csv::Writer::from_writer(io::stdout());
-    // Engine Loop
-    for month in 0..YEARS_TO_RUN {
-        // Handle Events
-        let current_events: Vec<&Event> = events.iter().filter(|event| event.month == month).collect();
-        if !current_events.is_empty() {
-            for current_event in current_events {
-                // Cashflow
-                cashflow.push( Cashflow { month: month, name: current_event.name.clone(), amount: current_event.cashflow });
-                // Assets
-                if current_event.asset {
-                    portfolio.push( Asset { month: month, name: current_event.name.clone(), value: current_event.debt });
-                }
-                // Debt
-                if current_event.debt != 0.0 {
-                    debt.push(Debt { month: month, amount: current_event.debt });
-                    let interest_payment = current_event.debt * MONTHLY_DEBT_INTEREST_RATE;
-                    // Add interest payments (negative) to cashflow
-                    cashflow.push( Cashflow { month: month, name: current_event.name.clone(), amount: -interest_payment });
-                }
-            }
-        }
-        
-        // Add monthly gains to portfolio
-        let mut total_portfolio_value: f32 = 0.0;
-        for asset in &portfolio {
-            total_portfolio_value += asset.value
-        }
-        let monthly_gain = total_portfolio_value * MONTHLY_ASSET_APPRECIATION;
-        portfolio.push( Asset { month: month, name: format!("Appreciation {}", month), value: monthly_gain });
 
-        // Divert excess cashflow to portfolio (securities)
-        let mut monthly_excess_cashflow:f32 = 0.0;
-        for val in &cashflow {
-            monthly_excess_cashflow += val.amount;
-        }
+    let monthly_outputs: Vec<MonthlyOutput> = engine::run_engine(70, cashflow, events, portfolio);
 
-        portfolio.push( Asset { month: month, name: format!("Excess Cashflow {}", month), value: monthly_excess_cashflow });
-
-        let portfolio_total = utils::sum_portfolio(&portfolio) as i64;
-        let debt_total = utils::sum_debt(&debt) as i64;
-        let output = MonthlyOutput {
-            month: month,
-            portfolio_sum: portfolio_total,
-            debt_sum: debt_total,
-            cashflow_sum: monthly_excess_cashflow as i64
-        };
-
-        std_wtr.serialize(&output).unwrap();
+    for month in monthly_outputs {
+        std_wtr.serialize(month).unwrap();
     }
     std_wtr.flush().unwrap()
 }
